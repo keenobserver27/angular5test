a823e9e1736e37749fdf41f35d18ef04b320498f
brylle barriga <keenobserver27@gmail.com>
2018-01-24 17:07:36 +0800

    initial testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  form;
+  /**
+   *
+   */
+  constructor() {
+    this.createForm();    
+  }
+
+  ngOnit() {
+    
+  }
+
+  createForm() {
+    this.form = new FormGroup({
+      customerName: new FormControl(),
+      location: new FormControl(),
+      details: new FormArray([
+      ])
+    });
+  }
+
+  details(): FormArray{ return this.form.get('details') as FormArray }
+
+  addDetail() {
+    this.details().push(
+    new FormGroup({
+      area: new FormControl(),
+      length: new FormControl(),
+      width: new FormControl(),
+      parts: new FormArray([
+        new FormGroup({
+          name: new FormControl(),
+          level: new FormControl()
+        })
+      ])
+    }))
+  }
+
+  deleteDetail(index){
+    this.details().removeAt(index);
+  }  
+
 }
